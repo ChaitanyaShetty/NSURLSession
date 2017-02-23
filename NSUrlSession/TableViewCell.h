47b61d5e833f8c32f67952a3b18a1581012f9f2a
ChaitanyaShetty <chaithanyavenk@gmail.com>
2017-02-23 04:33:54 -0200

    Initial commit

---
+++ NSUrlSession/TableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.h
+//  NSUrlSession
+//
+//  Created by chaitanya on 04/10/16.
+//  Copyright © 2016 chaitanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *lb1;
+@property (strong, nonatomic) IBOutlet UILabel *lb2;
+
+@property (strong, nonatomic) IBOutlet UIImageView *imgOutlet;
+
+@end
---
+++ NSUrlSession/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  NSUrlSession
+//
+//  Created by chaitanya on 02/10/16.
+//  Copyright © 2016 chaitanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+
+
+@end
+
